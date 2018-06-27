f4b5b5bbf6234cdbcf1de6d3d79f6c17b31fe7d2
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-06-26 23:56:49 -0500

    Add defaultProps

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -5,11 +5,21 @@ export interface MovieProps {
     id: number;
     title: string;
   };
+  desc: string;
 }
 
 class Movie extends React.Component<MovieProps> {
+  static defaultProps: Partial<MovieProps> = {
+    desc: 'Currently not available',
+  };
+
   render() {
-    return <div className="movie">{this.props.movie.title}</div>;
+    return (
+      <div>
+        <h3>{this.props.movie.title}</h3>
+        <p>{this.props.desc}</p>
+      </div>
+    );
   }
 }
 
