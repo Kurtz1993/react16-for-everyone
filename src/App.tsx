e377ac333c5b5c0bfec10e3b0818cba2f485360b
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-06-27 00:10:14 -0500

    Fetch movies from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
 import * as React from 'react';
 
-import Movie from '@app/components/Movie';
 import './App.css';
 import logo from './logo.svg';
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars',
-  },
-  {
-    id: 2,
-    title: 'Spider Man',
-  },
-  {
-    id: 3,
-    title: '36th Chamber of Shaolin',
-  },
-];
+import Movie from '@app/components/Movie';
 
 class App extends React.Component {
+  state = {
+    movies: [{}]
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=0d87075c2ae3f49df32c7eab2a4d0375&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+      );
+      const movies = await res.json();
+      this.setState({ movies: movies.results });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {this.state.movies.map((movie: any) => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
       </div>
     );
   }
