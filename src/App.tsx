85ee5b84dc0281c7c74b11080623020772b1fd2f
Luis Hernandez <luis.hlopez@outlook.com>
2018-06-27 14:56:17 -0500

    Simplify App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,22 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import logo from "./logo.svg";
 
-import Movie from "@app/components/Movie";
+import MoviesList from "@app/components/MoviesList";
 
-class App extends React.Component {
-  state = {
-    movies: [] as any[]
-  };
-
-  async componentDidMount() {
-    try {
-      const res = await fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=0d87075c2ae3f49df32c7eab2a4d0375&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
-      );
-      const movies = await res.json();
-      this.setState({ movies: movies.results });
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Route path="/test" component={Test} />
-          {this.state.movies.map((movie: any) => (
-            <Movie key={movie.id} movie={movie} />
-          ))}
-        </div>
-      </Router>
-    );
-  }
-}
-
-const Test = () => <h1>Test</h1>;
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Switch>
+        <Route path="/" component={MoviesList} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
