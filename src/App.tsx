c2f97babe0fd20efa9790244574909dbc9c9a363
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-06-26 23:00:00 -0500

    Add controlled input

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import './App.css';
 import logo from './logo.svg';
 
 class App extends React.Component {
+  state = {
+    input: 'Hello'
+  };
   inputRef = React.createRef<HTMLInputElement>();
 
   constructor(props: any) {
     super(props);
+  }
+
+  updateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ input: e.target.value });
   }
 
   submit = () => {
@@ -22,6 +29,11 @@ class App extends React.Component {
           <h1>Welcome to React</h1>
         </header>
 
+        <input
+          type="text"
+          onChange={this.updateInput}
+          value={this.state.input}
+        />
         <input type="text" ref={this.inputRef} />
         <button onClick={this.submit}>Show Value</button>
       </div>
